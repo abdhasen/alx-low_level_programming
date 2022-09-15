54c4d3dbe53187482e4b4ec768805b85d1ad2337
abdhasen <abdulhakim430@gmail.com>
2022-09-15 14:49:33 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,6 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month,int day);
-void print_remaining_days(int month,int day,int year);
-void print_to_98(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
