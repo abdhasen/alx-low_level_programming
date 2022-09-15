85b62c522d660c1b7abbe3ed76548ded8a33cbce
abdhasen <abdulhakim430@gmail.com>
2022-09-15 13:07:13 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -9,12 +9,12 @@
  */
 int _isdigit(int c)
 {
-if (c >= '0' && and c <= '9')
-{
-return (1);
-}
-else
-{
-return (0);
-}
+  if (c >= '0' && and c <= '9')
+    {
+      return (1);
+    }
+  else
+    {
+      return (0);
+    }
 }
