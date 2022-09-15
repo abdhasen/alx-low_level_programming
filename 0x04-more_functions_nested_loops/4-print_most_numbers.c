a64931338fd97b0e6e1f63c8d918f69f00342c2c
abdhasen <abdulhakim430@gmail.com>
2022-09-15 14:22:35 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_most_numbers - print numbers except 2 and 4.
@@ -11,7 +12,7 @@ for (i = '0'; i <= '9'; i++)
 {
   if ((i != '2') && (i != '4'))
 {
-_putchar(i);
+_putchar("%c",i);
 _putchar("\n");
 }
 }
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 
 /**
  * more_numbers - prints 0 -14 10x.
@@ -12,7 +12,7 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j <= 14; j++)
 {
-_putchar(j);
+_putchar"(%c", j);
 _putchar("\n");
 }
 }
