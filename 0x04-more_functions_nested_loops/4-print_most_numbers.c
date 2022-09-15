39548930e2741d64327ae4b14d1dce4b3e9cefc5
abdhasen <abdulhakim430@gmail.com>
2022-09-15 14:04:41 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "_putchar.c"
+
 /**
  * print_most_numbers - print numbers except 2 and 4.
  * Return: 0
@@ -9,11 +10,7 @@ void print_most_numbers(void)
 int i;
 for (i = '0'; i <= '9'; i++)
 {
-if (i == '2' || i == '4')
-{
-return (0);
-}
-else
+  if ((i != '2') || (i != '4'))
 {
 _putchar(i);
 _putchar("\n");
